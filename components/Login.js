5a9ec85a3765eab28affc25d31ef170467fca816
raj-subhankar <subhankar.rj@gmail.com>
2021-12-17 20:43:09 +0530

    Implement Login functionality

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
+
   return (
     <div className="bg-black relative">
       <h1>Login</h1>
@@ -11,7 +14,10 @@ function Login() {
           height={200}
           width={200}
         />
-        <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+        >
           Login
         </button>
       </div>
